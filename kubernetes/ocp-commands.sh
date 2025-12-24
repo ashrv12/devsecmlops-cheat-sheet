806a3811afffc69aec711b18f20eb45d3ab5bc9e
revvy <ariyashirav.b@m-bank.mn>
2025-12-24 11:50:20 +0800

    ocp edition

---
+++ kubernetes/ocp-commands.sh
@@ -0,0 +1,7 @@
+# creating a secret without specifying the namespace since the oc cli tool has to have a project selected to apply manifest files
+oc create secret docker-registry registry \
+  --docker-server=secureregistry.com:5000 \
+  --docker-username=admin \
+  --docker-password=123
+
+
