3157cdd23f85d7c576fb9b7385cfd6f1a734db5d
ari <bshirav@gmail.com>
2026-03-27 11:02:21 +0800

    full backup restore cycle elk

--- elk/useful.sh
+++ elk/useful.sh
@@ -1,6 +1,10 @@
+# get the elastic user password
 kubectl get secret elasticsearch-sample-es-elastic-user -o jsonpath='{.data.elastic}' | base64 -d
 
+# get all indices
+curl -k -u elastic:password -X GET "https://localhost:9200/_cat/indices?v"
 
+# register the new backup path
 curl -k -u elastic:password -X PUT "https://localhost:9200/_snapshot/pvc_backup" -H 'Content-Type: application/json' -d'
 > {
 >   "type": "fs",
@@ -8,11 +12,22 @@ curl -k -u elastic:password -X PUT "https://localhost:9200/_snapshot/pvc_backup"
 >     "location": "/usr/share/elasticsearch/backup"
 >   }
 > }'
-{"acknowledged":true}
 
-curl -k -u elastic:3jTTiywu1D0xqmBg2PclS2ok -X PUT "https://localhost:9200/_snapshot/pvc_backup/test_backup_01?wait_for_completion=true" -H 'Content-Type: application/json' -d'
+# RESPONSE
+# {"acknowledged":true}
+
+# Save the selected indices as test_backup_01 like a tar file at the previously registered fs file path pvc_backup
+curl -k -u elastic:password -X PUT "https://localhost:9200/_snapshot/pvc_backup/test_backup_01?wait_for_completion=true" -H 'Content-Type: application/json' -d'
 {
-  "indices": "index_1,index_2",
+  "indices": "retail-integration-cam-service-2026.03",
   "ignore_unavailable": true,
   "include_global_state": false
 }'
+
+# restores the elk index, remember to specify test_backup_01 or the name of the backup, remember to add the previously registered fs file path
+curl -k -u elastic:password -X POST /_snapshot/pvc_backup/test_backup_01/_restore
+{
+  "indices": "retail-integration-cam-service-2026.03",
+  "rename_pattern": "(.+)",
+  "rename_replacement": "restored_$1"
+}
